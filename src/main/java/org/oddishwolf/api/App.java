dc35b16b8d02d0583289c4df10470228fe05cac5
Dmitry Khorokhorin <bigbossdima98@mail.ru>
2022-11-04 02:05:13 +0300

    add findById() in UserDao

--- src/main/java/org/oddishwolf/api/App.java
+++ src/main/java/org/oddishwolf/api/App.java
@@ -4,12 +4,16 @@ import org.oddishwolf.api.dao.UserDao;
 import org.oddishwolf.api.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public class App {
     public static void main(String[] args) {
 
         List<User> users = UserDao.getInstance().findAll();
+        Optional<User> user = UserDao.getInstance().findById("username");
+
         System.out.println(users);
+        System.out.println(user);
 
     }
 }
--- src/main/java/org/oddishwolf/api/dao/UserDao.java
+++ src/main/java/org/oddishwolf/api/dao/UserDao.java
@@ -10,6 +10,7 @@ import org.oddishwolf.api.util.ConnectionManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +21,8 @@ public class UserDao implements Dao<String, User> {
     private static final UserDao INSTANCE = new UserDao();
 
     private static final String FIND_ALL_SQL = "SELECT username, first_name, last_name, birthday, email, g.gender_name " +
-            "FROM users JOIN gender g on users.gender = g.id;";
+            "FROM users JOIN gender g on users.gender = g.id";
+    private static final String FIND_BY_ID_SQL = FIND_ALL_SQL + " WHERE username = ?";
 
     @Override
     public User save(User entity) {
@@ -36,22 +38,26 @@ public class UserDao implements Dao<String, User> {
 
             List<User> users = new ArrayList<>();
             while (dbUsers.next()) {
-                users.add(User.builder()
-                        .username(dbUsers.getString("username"))
-                        .firstName(dbUsers.getString("first_name"))
-                        .lastName(dbUsers.getString("last_name"))
-                        .birthday(dbUsers.getDate("birthday").toLocalDate())
-                        .email(dbUsers.getString("email"))
-                        .gender(Gender.valueOf(dbUsers.getString("gender_name")))
-                        .build());
+                users.add(buildUser(dbUsers));
             }
             return users;
         }
     }
 
     @Override
+    @SneakyThrows
     public Optional<User> findById(String key) {
-        return Optional.empty();
+        try (Connection connection = ConnectionManager.open();
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            preparedStatement.setString(1, key);
+            ResultSet dbUser = preparedStatement.executeQuery();
+
+            User user = null;
+            if (dbUser.next()) {
+                user = buildUser(dbUser);
+            }
+            return Optional.ofNullable(user);
+        }
     }
 
     @Override
@@ -62,6 +68,17 @@ public class UserDao implements Dao<String, User> {
     @Override
     public boolean delete(String id) {
         return false;
+    }
+
+    private User buildUser(ResultSet dbUsers) throws SQLException {
+        return User.builder()
+                .username(dbUsers.getString("username"))
+                .firstName(dbUsers.getString("first_name"))
+                .lastName(dbUsers.getString("last_name"))
+                .birthday(dbUsers.getDate("birthday").toLocalDate())
+                .email(dbUsers.getString("email"))
+                .gender(Gender.valueOf(dbUsers.getString("gender_name")))
+                .build();
     }
 
     public static UserDao getInstance() {
