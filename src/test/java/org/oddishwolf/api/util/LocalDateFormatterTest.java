53d06b25f743b7803984917bd7b1d9b8dde2da15
Dmitry Khorokhorin <bigbossdima98@mail.ru>
2022-11-07 04:08:04 +0300

    very simple test for ScriptReader

--- src/test/java/org/oddishwolf/api/util/LocalDateFormatterTest.java
+++ src/test/java/org/oddishwolf/api/util/LocalDateFormatterTest.java
@@ -1,7 +1,6 @@
 package org.oddishwolf.api.util;
 
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EmptySource;
---
+++ src/test/java/org/oddishwolf/api/util/ScriptsReaderTest.java
@@ -0,0 +1,30 @@
+package org.oddishwolf.api.util;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Make sure that your properties changed for src/test/resources.
+ * After you can comment @Disabled.
+ */
+
+@Disabled
+@Tag("fast")
+@Tag("util_scriptsReader")
+public class ScriptsReaderTest {
+
+    @Test
+    void readScriptReturnCorrectNumberOfCharacters() {
+        String script = ScriptsReader.readScript("test_scripts_reader_1.txt");
+        assertThat(script).hasSize(935);
+    }
+
+    @Test
+    void readScriptReturnZeroNumberOfCharacters() {
+        String script = ScriptsReader.readScript("test_scripts_reader_2.txt");
+        assertThat(script).hasSize(0);
+    }
+}
